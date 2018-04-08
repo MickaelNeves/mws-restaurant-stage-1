026c5dc07ef07437e0498568165e9d1a6bef7cd0
DESKTOP-O8HSB72\Mickael <nevesmickael8@gmail.com>
2018-04-08 09:10:50 +0100

    Refactor sw

--- sw.js
+++ sw.js
@@ -1,86 +1,53 @@
-var cacheName = 'mws-ver2';
-var cacheImgs = 'mws-imgs-ver3';
+let staticCacheName = 'mws-restaurant-cache-v4';
+let urlsToCache = [
+    '/',
+    '/index.html',
+    '/data/restaurants.json',
+    '/restaurant.html',
+    '/css/styles.css',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/js/serviceworker_init.js',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg',
+];
 
-self.addEventListener('install',(event)=>{
-    var cacheFiles = [
-        '/',
-        'sw.js',
-        'restaurant.html',
-        'index.html',
-        'js/dbhelper.js',
-        'js/main.js',
-        'js/serviceworker_init.js',
-        'js/restaurant_info.js',
-        'css/styles.css',
-        'data/restaurants.json',
-        '/restaurant.html?id=1',
-        '/restaurant.html?id=2',
-        '/restaurant.html?id=3',
-        '/restaurant.html?id=4',
-        '/restaurant.html?id=5',
-        '/restaurant.html?id=6',
-        '/restaurant.html?id=7',
-        '/restaurant.html?id=8',
-        '/restaurant.html?id=9',
-        '/restaurant.html?id=10'
-    ];
-
+self.addEventListener('install', function (event) {
     event.waitUntil(
-        caches.open(cacheName).then((cache)=>{
-            console.log('Caching all images.');
-            return cache.addAll(cacheFiles);
+        caches.open(staticCacheName).then(function (cache) {
+            console.log('Caching...');
+            return cache.addAll(urlsToCache);
         })
     );
 });
 
-self.addEventListener('activate',(event)=>{
+self.addEventListener("activate", event => {
     event.waitUntil(
-        caches.keys().then((cacheNames)=>{
+        caches.keys().then(cacheNames => {
             return Promise.all(
-                cacheNames.filter((name)=>{
-                    return name.startsWith('mws-ver') &&
-                        name != cacheName;
-                }).map((name)=>{
-                    return caches.delete(name);
-                })
+                cacheNames.filter(cacheName => cacheName.startsWith("mws-restaurant") &&
+                    !staticCacheName.includes(cacheName))
+                    .map(cacheName => caches.delete(cacheName))
             );
-        })
-    )
+        }).catch(error => console.error("Error:", error))
+    );
 });
 
-cachedResponse = (event)=>{
-    let requestUrl = new URL(event.request.url);
-
-    if (requestUrl.pathname.startsWith('/img/')) {
-        return caches.open(cacheImgs).then(function(cache) {
-            return cache.match(requestUrl).then(function(response) {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request).then(function(networkResponse) {
-                    cache.put(requestUrl, networkResponse.clone());
-                    return networkResponse;
-                });
-            });
-        });
-    }
-
-    return caches.match(event.request).then((response)=>{
-        if(response) {
-            console.log('Grab from cache: ' + event.request.url);
-            return response;
-        }
-        return fetch(event.request).then((response)=>{
-            if(response.status == 404) {
-                return new Response("Error not found");
-            }
-            return response;
-        }).catch((err)=>{
-            console.log(`Failed to cache. Error: ${err}`);
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function (response) {
+            if (response) return response;
+            return fetch(event.request);
         })
-    })
-}
-
-self.addEventListener('fetch', (event)=>{
-    event.respondWith(cachedResponse(event));
+    );
 });
